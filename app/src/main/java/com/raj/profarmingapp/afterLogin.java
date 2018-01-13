771dfd0ef7bd634084bbcd84ac79ae9aa19808e5
Rajneesh Katkam <rajneesh.katkam@gmail.com>
2018-01-14 03:12:53 +0530

    Pie chart added to the app with real time data update from the firebase brings changes in the pie chart also

--- app/src/main/java/com/raj/profarmingapp/afterLogin.java
+++ app/src/main/java/com/raj/profarmingapp/afterLogin.java
@@ -3,19 +3,26 @@ package com.raj.profarmingapp;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Build;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -33,17 +40,23 @@ public class afterLogin extends AppCompatActivity {
     StorageReference mStorage;
     DatabaseReference mdatabaseReference;
     String uid;
+    PieChart pieChart;
+    int nitrogenPercent,pHPercent,potassiumPercent,phosphorousPercent;
+
 
 
 
     SharedPreferences file;  ///For storing flag value to check the external storage permissions and saving the flag value after the app is being shutdown;
-    Button logout;
+    Button logout,backChartButton;
     Button displayChart;
+    RelativeLayout chartRelativeLayout;
     ListView fieldInfoListView;
+    LinearLayout buttonLayout;
     //int gallery_Intent=1;
     ProgressDialog progressDialog;
     ArrayList<String> list;
     ArrayAdapter<String> adapter;
+    ArrayList<PieEntry> yvalues;
 
 
 
@@ -58,17 +71,29 @@ public class afterLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_login);
 
-
+        chartRelativeLayout=(RelativeLayout)findViewById(R.id.chartRelativeLayout);
+        buttonLayout=(LinearLayout)findViewById(R.id.buttonLayout);
         progressDialog=new ProgressDialog(this);
         fieldInfoListView=(ListView) findViewById(R.id.fieldInfoListView);
         list=new ArrayList<String>();
         adapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,list);
         logout=(Button) findViewById(R.id.logout);
         displayChart=(Button)findViewById(R.id.displayChart);
+        backChartButton=(Button)findViewById(R.id.backChartButton);
+        displayChart.setEnabled(false);
+        pieChart = (PieChart) findViewById(R.id.piechart);
+        pieChart.setUsePercentValues(true);
+        yvalues = new ArrayList<PieEntry>();
 
-
+        ///Storage of flag value after the app is killed or shutdown
         file = getSharedPreferences("save", 0);
         flag = Boolean.valueOf(file.getString("flag", "false"));
+        if (flag == false)////File permission check
+            checkFilePermissions();
+
+        SharedPreferences.Editor edit = file.edit();
+        edit.putString("flag", String.valueOf(flag));
+        edit.apply();
 
 
 
@@ -77,14 +102,6 @@ public class afterLogin extends AppCompatActivity {
         mStorage= FirebaseStorage.getInstance().getReference();
         mdatabaseReference= FirebaseDatabase.getInstance().getReference();
 
-
-
-        if (flag == false)////File permission check
-            checkFilePermissions();
-
-        SharedPreferences.Editor edit = file.edit();
-        edit.putString("flag", String.valueOf(flag));
-        edit.apply();
 
 
         ////Real time database update and control
@@ -100,6 +117,9 @@ public class afterLogin extends AppCompatActivity {
             }
         });
 
+
+
+
     }
 
 
@@ -109,7 +129,6 @@ public class afterLogin extends AppCompatActivity {
         if (mAuth.getCurrentUser() == null) {
 
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            //Toast.makeText(getApplicationContext(),"Welcome back",Toast.LENGTH_LONG).show();
             finish();
 
         }
@@ -183,6 +202,9 @@ public class afterLogin extends AppCompatActivity {
         list.add("Phosphorous: "+soilContent.phosphorous.toString());
         list.add("Potassium: "+soilContent.potassium.toString());
         fieldInfoListView.setAdapter(adapter);
+        displayChart.setEnabled(true);
+        drawChart();
+
 
     }
 
@@ -195,6 +217,69 @@ public class afterLogin extends AppCompatActivity {
         finish();
         makeToast("Logged out");
     }
+
+    public void percentage()
+    {
+        int total =soilContent.nitrogen+soilContent.pH+soilContent.phosphorous+soilContent.potassium;
+        nitrogenPercent=soilContent.nitrogen*100/total;
+        pHPercent=soilContent.pH*100/total;
+        potassiumPercent=soilContent.potassium*100/total;
+        phosphorousPercent=soilContent.phosphorous*100/total;
+
+    }
+
+
+
+    public void drawChart()
+    {
+        percentage();
+        yvalues.clear();
+        yvalues.add(new PieEntry(nitrogenPercent, "Nitrogen",0));
+        yvalues.add(new PieEntry(pHPercent, "pH Value",1));
+        yvalues.add(new PieEntry(potassiumPercent, "Potassium",2));
+        yvalues.add(new PieEntry(phosphorousPercent, "Phosphorous",3));
+
+        PieDataSet dataSet = new PieDataSet(yvalues, "");
+        PieData data = new PieData(dataSet);
+        data.setValueTextColor(Color.BLACK);
+        data.setValueFormatter(new PercentFormatter());
+        pieChart.setData(data);
+        pieChart.invalidate();
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        Description description=new Description();
+        description.setText("Soil nutrients Information");
+        description.setTextSize(12f);
+        pieChart.setDescription(description);
+        pieChart.setEntryLabelTextSize(15f);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        data.setValueTextSize(20f);
+        pieChart.setDrawHoleEnabled(false);
+
+
+
+
+
+    }
+
+
+    void displayChart(View v)
+    {
+
+        chartRelativeLayout.setVisibility(View.VISIBLE);
+        fieldInfoListView.setVisibility(View.INVISIBLE);
+        buttonLayout.setVisibility(View.INVISIBLE);
+
+    }
+
+    void backChart(View v)
+    {
+        chartRelativeLayout.setVisibility(View.INVISIBLE);
+        fieldInfoListView.setVisibility(View.VISIBLE);
+        buttonLayout.setVisibility(View.VISIBLE);
+
+    }
+
+
 
 
 
